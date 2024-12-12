d7d5ce99b45e3ffecdec550ebda1ad2749f8d94a
FerminEncalada <jose.f.encalada.l@unl.edu.ec>
2024-12-12 01:19:15 -0500

    rutas

--- Frontend/app.py
+++ Frontend/app.py
@@ -4,7 +4,7 @@ def create_app():
     app = Flask(__name__, template_folder='templates', instance_relative_config=False)
     app.secret_key = 'Gestor_cine'
 
-    from routes.route import router
-    app.register_blueprint(router)
+    from routes.route import route
+    app.register_blueprint(route)
 
     return app
--- Frontend/routes/route.py
+++ Frontend/routes/route.py
@@ -1,1 +1,13 @@
 import json
+from flask import Flask, Blueprint, render_template, request, redirect, url_for, flash, session
+import requests
+
+route = Blueprint('route', __name__)
+
+@route.route('/')
+def home():
+    return render_template('index.html')
+
+@route.route('inicio')
+def inicio():
+    return render_template('inicio.html')
